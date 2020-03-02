0ebc782d769a4a4669f1371cd64f4640f951151e
cigalat14 <ignsae@alu.iesserpis.org>
2020-03-02 13:34:33 +0100

    bingo acabado

---
+++ BingoCiga/BingoCiga/BingoCiga/MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+using BingoCiga;
+using CTabla;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Panel Panelbingo = new Panel(vbox2);
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
---
+++ BingoCiga/BingoCiga/BingoCiga/Panel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Gtk;
+
+namespace CTabla
+{
+    public class Panel
+    {
+        public Panel(VBox vbox1)
+        {
+            {
+                Table table = new Table(10, 9, true);
+                int index = 0;
+                for (int row = 0; row < 10; row++)
+                    for (int column = 0; column < 9; column++)
+                    {
+                        index++;
+                        Button button = new Button();
+                        table.Attach(button, (uint)column, (uint)column + 1, (uint)row, (uint)row + 1);
+                        //button.Label = "Button ";
+                        button.Label =index.ToString();
+                        button.Clicked += delegate {
+                            button.ModifyBg(StateType.Normal, new Gdk.Color(100, 200, 250));
+
+                        };
+                    }
+                vbox1.Add(table);
+                table.ShowAll();
+            }
+        }
+    }
+}
